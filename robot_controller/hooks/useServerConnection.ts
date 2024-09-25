40e16948da242a0153a2932292ed4a62b5329979
Ethan Hutchins <ethanjameshutchins@gmail.com>
2024-09-25 13:11:48 +0100

    feat: Get /robot_description from rosbridge server

--- robot_controller/hooks/useServerConnection.ts
+++ robot_controller/hooks/useServerConnection.ts
@@ -1,10 +1,12 @@
 import * as roslib from "roslib";
 
 export function useServerConnection() {
-    const ros = new roslib.Ros({
+    let robot_description: Object;
+
+    const ros: roslib.Ros = new roslib.Ros({
         url: "ws://192.168.1.95:9090",
         transportLibrary: "websocket",
-      }); 
+    }); 
 
     ros.on("connection", () => {
         console.log("Connected to ws server ejaitch");
@@ -12,9 +14,24 @@ export function useServerConnection() {
 
     ros.on("close", () => {
         console.log("Connection to ws server cancelled ejaitch");
-    })
+    });
 
     ros.on("error", () => {
         console.log("Error when connecting to ws server ejaitch");
     });
+
+    const description_param: roslib.Param = new roslib.Param({
+        ros: ros,
+        name: "robot_description"
+    });
+
+    description_param.get(
+        (value: Object) => {
+            robot_description = value;
+            console.log(JSON.stringify(value));
+        }, 
+        (error: string) => {
+            console.error(error);
+        }
+    );
 }
