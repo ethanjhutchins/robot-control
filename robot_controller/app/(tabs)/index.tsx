d073fb086ebb94564f4b53d30295079a311392e6
Ethan Hutchins <ethanjameshutchins@gmail.com>
2024-09-26 15:41:31 +0100

    feat: Get robot description and displayon screen

--- robot_controller/app/(tabs)/index.tsx
+++ robot_controller/app/(tabs)/index.tsx
@@ -1,19 +1,28 @@
-import { useServerConnection } from '@/hooks/useServerConnection';
-import { useEffect } from 'react';
+import RosContext from '@/components/RosContext';
+import ServerConnection from '@/hooks/useServerConnection';
+import { useContext, useEffect, useState } from 'react';
 import { Dimensions, SafeAreaView, Text, View } from 'react-native';
 
 export default function MotionScreen() {
-
   const { height, width } = Dimensions.get("window");
+  const { ros, isConnected } = useContext(RosContext);
+  const [robot_description, setRobotDescription] = useState<Object>({});
 
   useEffect(() => {
-    useServerConnection()
-  }, [])
+    const getRobotDescription = async () => {
+      if (isConnected && !robot_description) {
+        const desc = await ServerConnection.getRobotDescription(ros);
+        setRobotDescription(desc);
+      }
+    }
+    getRobotDescription()
+  }, [isConnected])
 
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center' }}>
       <View style={{ flex: 1, justifyContent: 'center', borderColor: 'white' }}>
-        <Text style={{ color: 'white', textAlign: 'center', }}>ros3djs visualiser</Text>
+        <Text style={{ color: 'white', textAlign: 'center', }}>{JSON.stringify(robot_description)}</Text>
+        {/* <Text style={{ color: 'white', textAlign: 'center', }}>ros3djs visualiser</Text> */}
       </View>
       <View style={{ flex: 1, justifyContent: 'center', borderColor: 'white' }}>
         <Text style={{ color: 'white', textAlign: 'center', }}>sliders</Text>
