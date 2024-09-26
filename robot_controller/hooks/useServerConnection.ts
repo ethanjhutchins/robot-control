3d53c316a8d7b6ba403477f4620968e19baedece
Ethan Hutchins <ethanjameshutchins@gmail.com>
2024-09-26 15:41:14 +0100

    feat: Make ServerConnection class containing static method getRobotDescription

--- robot_controller/hooks/useServerConnection.ts
+++ robot_controller/hooks/useServerConnection.ts
@@ -1,37 +1,24 @@
 import * as roslib from "roslib";
 
-export function useServerConnection() {
-    let robot_description: Object;
+export default class ServerConnection {
 
-    const ros: roslib.Ros = new roslib.Ros({
-        url: "ws://192.168.1.95:9090",
-        transportLibrary: "websocket",
-    }); 
+    static async getRobotDescription(ros: roslib.Ros): Promise<Object> {
 
-    ros.on("connection", () => {
-        console.log("Connected to ws server ejaitch");
-    });
+        const description_param: roslib.Param = new roslib.Param({
+            ros: ros,
+            name: "robot_description"
+        });
 
-    ros.on("close", () => {
-        console.log("Connection to ws server cancelled ejaitch");
-    });
-
-    ros.on("error", () => {
-        console.log("Error when connecting to ws server ejaitch");
-    });
-
-    const description_param: roslib.Param = new roslib.Param({
-        ros: ros,
-        name: "robot_description"
-    });
-
-    description_param.get(
-        (value: Object) => {
-            robot_description = value;
-            console.log(JSON.stringify(value));
-        }, 
-        (error: string) => {
-            console.error(error);
-        }
-    );
+        return new Promise( (resolve, reject) => {
+            description_param.get(
+                (value: Object) => {
+                    resolve(value);
+                }, 
+                (error: string) => {
+                    console.error(error);
+                    reject(error);
+                }
+            );
+        });
+    }
 }
